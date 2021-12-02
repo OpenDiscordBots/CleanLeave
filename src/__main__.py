8dfc9de7918ec48149a3283e262dbf4c10f7705f
vcokltfre <vcokltfre@gmail.com>
2021-12-02 10:02:04 +0000

    chore: switch to bauxite

--- src/__main__.py
+++ src/__main__.py
@@ -1,30 +1,29 @@
+from asyncio import run
 from dotenv import load_dotenv
 from os import environ as env
 
-from corded import CordedClient, GatewayEvent, Route
+from bauxite import HTTPClient, GatewayClient, Route
 from loguru import logger
 
 from .db import Database
 
 load_dotenv()
 
-bot = CordedClient(env["TOKEN"], intents=515)
+http = HTTPClient(env["TOKEN"])
 db = Database()
 
-@bot.on("message_create")
-async def on_message_create(event: GatewayEvent) -> None:
-    if event.d["type"] == 7:
-        message = int(event.d["id"])
-        channel = int(event.d["channel_id"])
-        guild = int(event.d["guild_id"])
-        member = int(event.d["author"]["id"])
+async def on_message_create(data: dict) -> None:
+    if data["type"] == 7:
+        message = int(data["id"])
+        channel = int(data["channel_id"])
+        guild = int(data["guild_id"])
+        member = int(data["author"]["id"])
 
         await db.member_join(message, channel, guild, member)
 
-@bot.on("guild_member_remove")
-async def on_member_leave(event: GatewayEvent) -> None:
-    guild = int(event.d["guild_id"])
-    member = int(event.d["user"]["id"])
+async def on_member_leave(data: dict) -> None:
+    guild = int(data["guild_id"])
+    member = int(data["user"]["id"])
 
     msg = await db.get_join_message(guild, member)
 
@@ -33,14 +32,31 @@ async def on_member_leave(event: GatewayEvent) -> None:
 
     logger.info(f"Removing join message for {member} in guild {guild}")
 
-    route = Route("/channels/{channel_id}/messages/{message_id}", message_id=msg[0], channel_id=msg[1])
+    route = Route("DELETE", "/channels/{channel_id}/messages/{message_id}", message_id=msg[0], channel_id=msg[1])
 
-    await bot.http.request("DELETE", route, expect="response")
+    await http.request(route)
+
+async def dispatch(_shard, _direction, data: dict) -> None:
+    if not (t := data.get("t")):
+        return
+
+    if t == "MESSAGE_CREATE":
+        await on_message_create(data)
+        return
+
+    if t == "GUILD_MEMBER_REMOVE":
+        await on_member_leave(data)
+        return
+
+async def main() -> None:
+    await db.ainit()
+
+    gateway = GatewayClient(http, 515, callbacks=[dispatch])
+
+    await gateway.spawn_shards()
 
 
 if __name__ == "__main__":
-    bot.loop.run_until_complete(db.ainit())
-
     logger.info("Starting the bot...")
 
-    bot.start()
+    run(main())
